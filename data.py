64b2acbd7d1f2665d356ae5fb4a78db36b080961
Markmanus <pampadalampa@gmail.com>
2023-08-09 00:05:13 +0100

    43wg34

--- data.py
+++ data.py
@@ -1,8 +1,19 @@
 import requests
+
+
+parameters = {
+    "amount": 10,
+    "category": 18,
+    "difficulty": "medium",
+    "type": "boolean",
+}
 response = requests.get(
 
-    "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=boolean"
+    "https://opentdb.com/api.php", params=parameters
 )
+response.raise_for_status()
+
+
 question_data = response.json()["results"]
 
 
--- ui.py
+++ ui.py
@@ -1,2 +1,40 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        ## Canvas middle ##
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+        ##buttons##
+        image_true = PhotoImage(file="images/true.png")
+        image_false = PhotoImage(file="images/false.png")
+        self.image_true = Button(image=image_true, highlightthickness=0)
+        self.image_false = Button(image=image_false, highlightthickness=0)
+        self.image_true.grid(row=2, column=0)
+        self.image_false.grid(row=2, column=1)
+        #score label##
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.get_next_question()
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
