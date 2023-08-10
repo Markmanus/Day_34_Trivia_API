42d4a80a0f6b669c9a91794a5019e6ba9875eac8
Markmanus <pampadalampa@gmail.com>
2023-08-10 20:20:19 +0100

    43wg34

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,4 +1,6 @@
 import html
+import random
+from tkinter import *
 class QuizBrain:
 
 
@@ -23,9 +25,7 @@ class QuizBrain:
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
+            return False
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
